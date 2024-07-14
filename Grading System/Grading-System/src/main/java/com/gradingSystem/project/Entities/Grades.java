c5eed02f824834b14638f86d954d7aafcf468912
noddle-Cake <chris.servin001@gmail.com>
2024-07-14 00:23:50 -0400

    Make/Port simple classes for these based off the previous C code

---
+++ Grading System/Grading-System/src/main/java/com/gradingSystem/project/Entities/Grades.java
@@ -0,0 +1,55 @@
+package com.gradingSystem.project.Entities;
+
+public class Grades {
+    private int classCode;
+    private char gradeLetter;
+    private float percentage;
+
+    // Getters and setters
+    public int getClassCode() {
+        return classCode;
+    }
+
+    public void setClassCode(int classCode) {
+        this.classCode = classCode;
+    }
+
+    public char getGradeLetter() {
+        return gradeLetter;
+    }
+
+    public void setGradeLetter(char gradeLetter) {
+        this.gradeLetter = gradeLetter;
+    }
+
+    public float getPercentage() {
+        return percentage;
+    }
+
+    public void setPercentage(float percentage) {
+        this.percentage = percentage;
+        this.gradeLetter = percentageToLetter(percentage);
+    }
+
+    private char percentageToLetter(float percentage) {
+        if (percentage >= 95) {
+            return 'A';
+        } else if (percentage >= 90) {
+            return 'A';
+        } else if (percentage >= 87) {
+            return 'B';
+        } else if (percentage >= 83) {
+            return 'B';
+        } else if (percentage >= 80) {
+            return 'B';
+        } else if (percentage >= 77) {
+            return 'C';
+        } else if (percentage >= 70) {
+            return 'C';
+        } else if (percentage >= 60) {
+            return 'D';
+        } else {
+            return 'F';
+        }
+    }
+}
---
+++ Grading System/Grading-System/src/main/java/com/gradingSystem/project/Entities/Student.java
@@ -0,0 +1,12 @@
+package com.gradingSystem.project.Entities;
+
+import java.util.List;
+
+public class Student{
+    private int pantherID;
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String email;
+    private List<Grades> Grades;
+}
